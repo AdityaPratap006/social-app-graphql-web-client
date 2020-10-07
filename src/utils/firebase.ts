031256c26eaf3846923ed3d96df20243e087c0d3
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-07 18:01:26 +0530

    setup firebase for authentication

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjeA4yA_jGaWqlCMmSVTSRFREYhK-dl9A",
+    authDomain: "social-media-app-graphql.firebaseapp.com",
+    // databaseURL: "https://social-media-app-graphql.firebaseio.com",
+    projectId: "social-media-app-graphql",
+    storageBucket: "social-media-app-graphql.appspot.com",
+    // messagingSenderId: "51154739422",
+    appId: "1:51154739422:web:80513a44db999a2e3b624f",
+    measurementId: "G-ZCNS2GGNE0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseAuth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
