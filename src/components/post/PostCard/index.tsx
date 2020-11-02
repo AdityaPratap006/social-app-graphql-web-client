2bb4041ae5cb8ecedbf02c6bad46cb6dfda95870
Aditya Pratap <adityapratap7344@gmail.com>
2020-11-02 05:44:03 +0530

    added author details in post, added a start:prod script

--- src/components/post/PostCard/index.tsx
+++ src/components/post/PostCard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PostTitle, PostContent, PostCardStyles } from './style';
+import { PostTitle, PostContent, PostHeader, PostAuthorDetails, PostAuthorImage, PostAuthorName, PostAuthorEmail } from './style';
 import { IPost } from '../../../models';
 import Card from '../../shared/Card';
 
@@ -9,10 +9,23 @@ interface PostCardProps {
 
 const PostCard: React.FC<PostCardProps> = (props) => {
     const { post } = props;
+    const {
+        title,
+        description,
+        createdBy: author,
+    } = post;
+
     return (
-        <Card addCSS={PostCardStyles}>
-            <PostTitle>{post.title}</PostTitle>
-            <PostContent>{post.description}</PostContent>
+        <Card>
+            <PostHeader>
+                <PostAuthorImage src={author.images[0].url} />
+                <PostAuthorDetails>
+                    <PostAuthorName>{author.name}</PostAuthorName>
+                    <PostAuthorEmail>{author.email}</PostAuthorEmail>
+                </PostAuthorDetails>
+            </PostHeader>
+            <PostTitle>{title}</PostTitle>
+            <PostContent>{description}</PostContent>
         </Card>
     );
 };
--- src/components/post/PostCard/style.tsx
+++ src/components/post/PostCard/style.tsx
@@ -1,7 +1,39 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-export const PostCardStyles = css`
+export const PostHeader = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`;
 
+export const PostAuthorImage = styled.img`
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    margin: 0rem;
+`;
+
+export const PostAuthorDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 0 1rem;
+`;
+
+export const PostAuthorName = styled.h5`
+    padding: 0;
+    margin: 0.4rem 0;
+    font-size: 1rem;
+    color: ${props => props.theme.primary};
+`;
+
+export const PostAuthorEmail = styled.small`
+    padding: 0;
+    margin: 0.2rem 0;
+    font-size: 0.8rem;
+    color: ${props => props.theme.text};
 `;
 
 export const PostTitle = styled.h4`
